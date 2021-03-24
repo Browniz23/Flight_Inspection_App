78139abc7d651948e549943c950c5b45881d658b
oziyo <oziyon@gmail.com>
2021-03-24 14:48:03 +0200

    delete unnecessary end

--- Flight_Inspection_App/Connect.cs
+++ Flight_Inspection_App/Connect.cs
@@ -37,16 +37,18 @@ namespace Flight_Inspection_App
                 {
                     // Translate the passed message into ASCII and store it as a Byte array.
                     Byte[] data = System.Text.Encoding.ASCII.GetBytes(lines[i]);
-                    Byte[] data2 = System.Text.Encoding.ASCII.GetBytes("\n");
+                    Byte[] newline = System.Text.Encoding.ASCII.GetBytes("\n");
                     // Send the message to the connected TcpServer.
                     stream.Write(data, 0, data.Length);
-                    stream.Write(data2, 0, data2.Length);
+                    // adds new line for FG. in java code add new line automaticlly.
+                    stream.Write(newline, 0, newline.Length);
+
+                    // JUST FOR NOW: prints all lines in console.
                     Console.WriteLine("Sent: {0}", lines[i]);
 
                     stream.Flush();              // TODO: i added. needed?
                     Thread.Sleep(100);
                 }
-
 
                 // Close everything.
                 stream.Close();
@@ -60,92 +62,6 @@ namespace Flight_Inspection_App
             {
                 Console.WriteLine("SocketException: {0}", e);
             }
-
-            Console.WriteLine("\n Press Enter to continue...");
-            Console.Read();
-       
-            try
-            {
-                // Establish the remote endpoint  
-                // for the socket. This example  
-                // uses port 11111 on the local  
-                // computer. 
-                IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
-                ipHost.AddressList[0] = IPAddress.Parse("127.0.0.1");
-                IPAddress ipAddr = ipHost.AddressList[0];
-                IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 5400);
-
-                // Creation TCP/IP Socket using  
-                // Socket Class Costructor 
-                Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
-                try
-                {
-
-                    // Connect Socket to the remote  
-                    // endpoint using method Connect() 
-                    sender.Connect(localEndPoint);
-
-                    // We print EndPoint information  
-                    // that we are connected 
-                    Console.WriteLine("Socket connected to -> {0} ", sender.RemoteEndPoint.ToString());
-
-                    // Creation of messagge that 
-                    // we will send to Server 
-                    byte[] messageSent;
-                    int byteSent;
-
-                    for (int i = 0; i < lines.Length; i++)
-                    {
-                        Console.WriteLine(lines[i]);
-                        messageSent = Encoding.ASCII.GetBytes(lines[i]);
-                        byteSent = sender.Send(messageSent);
-                        Thread.Sleep(100);
-                    }
-                    // Data buffer 
-                    byte[] messageReceived = new byte[1024];
-
-                    /*
-                    // We receive the messagge using  
-                    // the method Receive(). This  
-                    // method returns number of bytes 
-                    // received, that we'll use to  
-                    // convert them to string 
-                    int byteRecv = sender.Receive(messageReceived);
-                    Console.WriteLine("Message from Server -> {0}",
-                          Encoding.ASCII.GetString(messageReceived,
-                                                     0, byteRecv));
-                    */
-                    // Close Socket using  
-                    // the method Close() 
-                    sender.Shutdown(SocketShutdown.Both);
-                    sender.Close();
-                }
-
-                // Manage of Socket's Exceptions 
-                catch (ArgumentNullException ane)
-                {
-
-                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
-                }
-
-                catch (SocketException se)
-                {
-
-                    Console.WriteLine("SocketException : {0}", se.ToString());
-                }
-
-                catch (Exception e)
-                {
-                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
-                }
-            }
-
-            catch (Exception e)
-            {
-
-                Console.WriteLine(e.ToString());
-            }
-        } 
+        }
     }
 }
